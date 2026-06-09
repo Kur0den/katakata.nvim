0629718ae4e611532adec172da706c23a94a36a4
Kur0den <git@kur0den.net>
2026-06-10 03:27:14 +0900

    なんかこういい感じに調整

--- lua/katakata/init.lua
+++ lua/katakata/init.lua
@@ -4,18 +4,31 @@ local type_sound_count = 4
 local last_type_time = 0
 local type_threshold = 15
 
---@alias typesound "normal" | "space" | "indent" | "enter"
+--@alias typesound "normal" | "space" | "tab" | "return"
 
 --@param type typesound
-local function play_sound(type)
-  local file_name = string.format("type%02d.wav", math.random(type_sound_count))
-  local sound_path = "./audio/" .. file_name
+local function type_sound(type)
+  local sound_path = nil
+  if (type ~= "space") then
+    local file_name = string.format("type%02d.wav", math.random(type_sound_count))
+    sound_path = "./audio/" .. file_name
+  else
+    sound_path = "./audio/" .. "space.wav"
+  end
   sound_path = vim.fn.fnamemodify(sound_path, ":p")
-  vim.system({"aplay", sound_path}, 
-    function(job)
-    end
-  )
+  vim.system({"aplay", sound_path})
+  
+  if (type == "return" or type == "tab") then
+    sound_path = "./audio/" .. type .. ".wav"
+    sound_path = vim.fn.fnamemodify(sound_path, ":p")
+    vim.system({"aplay", sound_path})
+  end
+end
 
+local function bell_sound()
+  local sound_path = "./audio/" .. type .. ".wav"
+  sound_path = vim.fn.fnamemodify(sound_path, ":p")
+  vim.system({"aplay", sound_path})
 end
 
 
@@ -26,16 +39,29 @@ function M.setup()
   vim.on_key(function(key)
     if key == "" then return end
     
-    local current_time = vim.loop.hrtime() / 1000000
+    local mode = vim.api.nvim_get_mode().mode
+    vim.notify("mode: " .. mode, vim.log.levels.INFO)
 
+    if not (mode == "i" or mode == "c" or mode == "t") then return end
+
+    local current_time = vim.loop.hrtime() / 1000000
     if (current_time - last_type_time) < type_threshold then
       last_type_time = current_time
       return
     end
     
     last_type_time = current_time
-
-    play_sound("normal")
+    
+    local k = vim.fn.keytrans(key)
+    if k == "<CR>" then
+      type_sound("return")
+    elseif k == "<Space>" then
+      type_sound("space")
+    elseif k == "<Tab>" then
+      type_sound("tab")
+    else
+      type_sound("normal")
+    end
   end, ns)
 
   print("Plugin loaded!")
